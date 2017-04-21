ed96c57e4ae55e5ddd5f275fa067ab0b684c6af1
정유진 <yjjeong@gnclouds.com>
2017-04-21 14:14:10 +0900

    검색바 header 전체 수정

---
+++ publish/assets/gn_js/searchnmenu.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    var submitIcon = $('.searchbox-icon');
+    var inputBox = $('.searchbox-input');
+    var searchBox = $('.searchbox');
+    var isOpen = false;
+    submitIcon.click(function(){
+        if(isOpen == false){
+            searchBox.addClass('searchbox-open');
+            inputBox.focus();
+            isOpen = true;
+        } else {
+            searchBox.removeClass('searchbox-open');
+            inputBox.focusout();
+            isOpen = false;
+        }
+    });
+    submitIcon.mouseup(function(){
+        return false;
+    });
+    searchBox.mouseup(function(){
+        return false;
+    });
+    $(document).mouseup(function(){
+        if(isOpen == true){
+            $('.searchbox-icon').css('display','block');
+            submitIcon.click();
+        }
+    });
+});
+function buttonUp(){
+    var inputVal = $('.searchbox-input').val();
+    inputVal = $.trim(inputVal).length;
+    if( inputVal !== 0){
+        $('.searchbox-icon').css('display','none');
+    } else {
+        $('.searchbox-input').val('');
+        $('.searchbox-icon').css('display','block');
+    }
+}
+
+
+/*left menu*/
+$(".btn").click(function() {
+    $("#menu,.page_cover,html").addClass("open");
+    window.location.hash = "#open";
+});
+
+window.onhashchange = function() {
+    if (location.hash != "#open") {
+        $("#menu,.page_cover,html").removeClass("open");
+    }
+};
+new UISearch( document.getElementById( 'sb-search' ) );
